4a5db94029951bb1756c9bdce2069de263241bff
Jordan <113162152+1202-Ledesma-Jordan@users.noreply.github.com>
2023-12-04 11:51:40 -0800

    Update waterSensor.ino
    - change delay to myDelay function
    - deleted commented out code

--- waterSensor.ino
+++ waterSensor.ino
@@ -1,7 +1,3 @@
-// Sensor pins
-//#define sensorPower 7
-#define sensorPin A0
-
 #define RDA 0x80
 #define TBE 0x20  
 
@@ -20,6 +16,13 @@ volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+// timer pointers
+volatile unsigned char *myTCCR1A = (unsigned char *) 0x80;
+volatile unsigned char *myTCCR1B = (unsigned char *) 0x81;
+volatile unsigned char *myTCCR1C = (unsigned char *) 0x82;
+volatile unsigned char *myTIMSK1 = (unsigned char *) 0x6F;
+volatile unsigned int  *myTCNT1  = (unsigned  int *) 0x84;
+volatile unsigned char *myTIFR1 =  (unsigned char *) 0x36;
 
 // Value for storing water level
 int waterLevel = 0;
@@ -29,22 +32,19 @@ void setup() {
 	//setup water level sensor
   *ddr_f |= 0x01; //output
   Serial.begin(9600);
-  //Serial.begin(9600);
   adc_init();
 }
 
 void loop() {
-  //int level = readWaterLevel();
   int level2 = readWaterLevelADC(); 
 	
   if(i%2 == 1){
-	  Serial.print("Water level: ");
-	  //Serial.println(level);
+	  Serial.print("Water level: "); // sensor is only correct every other reading
 	  Serial.println(level2);
   }
   i++;
 
-	delay(1000);
+	myDelay(1000);
 }
 
 
@@ -54,23 +54,8 @@ int readWaterLevelADC(){
   *pin_f |= 0x01;
 
   //read data
-  delay(10);						            	// wait 10 milliseconds
+  myDelay(10);						            	// wait 10 milliseconds
 	waterLevel = adc_read(0);		  // Read the analog value form sensor
-  
-  //turn off
-  *pin_f &= 0xFE;
-
-  return waterLevel;
-}
-
-int readWaterLevel(){
-  int data;
-  //turn on
-  *pin_f |= 0x01;
-
-  //read data
-  delay(10);						            	// wait 10 milliseconds
-	waterLevel = analogRead(sensorPin);		  // Read the analog value form sensor
   
   //turn off
   *pin_f &= 0xFE;
@@ -142,4 +127,29 @@ unsigned int adc_read(unsigned char adc_channel_num)
   while((*my_ADCSRA & 0x40) != 0);
   // return the result in the ADC data register
   return *my_ADC_DATA;
+}
+
+void myDelay(unsigned int milliseconds) {
+  int freq = (1/milliseconds)*1000;
+  // calc period
+  double period = 1.0/double(freq);
+  // 50% duty cycle
+  double half_period = period/ 2.0f;
+  // clock period def
+  double clk_period = 0.0000000625;
+  // calc ticks
+  unsigned int ticks = half_period / clk_period;
+  // stop the timer
+  *myTCCR1B &= 0xF8;
+  // set the counts
+  *myTCNT1 = (unsigned int) (65536 - ticks);
+  // start the timer
+  * myTCCR1A = 0x0;
+  * myTCCR1B |= 0b00000001;
+  // wait for overflow
+  while((*myTIFR1 & 0x01)==0); // 0b 0000 0000
+  // stop the timer
+  *myTCCR1B &= 0xF8;   // 0b 0000 0000
+  // reset TOV           
+  *myTIFR1 |= 0x01;
 }
